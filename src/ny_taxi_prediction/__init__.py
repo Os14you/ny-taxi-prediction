a699f35bcc07a61026c564e042c872e57180e356
Osama <osama.yousef@tuta.io>
2025-11-20 10:32:29 +0200

    feat: provide the __init__ to make it easy for imports

--- src/ny_taxi_prediction/__init__.py
+++ src/ny_taxi_prediction/__init__.py
@@ -1,2 +1,30 @@
-def hello() -> str:
-    return "Hello from ny-taxi-prediction!"
+from .data import load_data, clean_data
+from .features import (
+    add_temporal_features,
+    add_anomaly_feature,
+    add_trip_distance,
+    add_traffic_congestion,
+    add_airport_features
+)
+from .modeling import (
+    split_data,
+    preprocess_features,
+    train_model,
+    evaluate_model
+)
+from .utils import load_config
+
+__all__ = [
+    "load_data",
+    "clean_data",
+    "add_temporal_features",
+    "add_anomaly_feature",
+    "add_trip_distance",
+    "add_traffic_congestion",
+    "add_airport_features",
+    "split_data",
+    "preprocess_features",
+    "train_model",
+    "evaluate_model",
+    "load_config",
+]
